691f832442e8048cf363c78f279b9f177224251f
Vinay Chandola <72332260+Hero-debug-cloud@users.noreply.github.com>
2024-02-29 23:37:09 +0530

    Update variables.js

--- client/variables.js
+++ client/variables.js
@@ -1,3 +1,4 @@
-const endpoint = "https://no-code-backend-nirl.onrender.com";
+// const endpoint = "https://no-code-backend-nirl.onrender.com";
 // const endpoint = "http://localhost:9000";
+const endpoint="https://13.50.25.86:9000"
 export default endpoint;
